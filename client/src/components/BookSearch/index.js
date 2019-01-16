ec88e6e3ef7f1595038473b7de52a692a7175c04
LealTX <jacob.leal32@gmail.com>
2019-01-16 17:54:30 -0600

    Fixing Save Book Function

--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -5,44 +5,44 @@ import API from "../../utils/API";
 class BookSearch extends Component {
     constructor(props) {
         super(props);
-        this.state = {book: {}}
+        this.state = { book: {} }
     }
 
 
-    details = event => {
+    bookDetails = event => {
         event.preventDefault();
         window.open(this.props.link);
-        }
+    }
 
     handleBookSave = index => {
         index.preventDefault();
-        const newBook = {
-            title: this.state.book.title,
-            authors: this.state.book.authors,
-            thumbnail: this.state.book.thumbnail,
-            synopsis: this.state.book.synopsis
-        }
-        API.saveBook(newBook)
+        API.saveBook({
+            title: this.props.title,
+            authors: this.props.authors,
+            thumbnail: this.props.thumbnail,
+            synopsis: this.props.synopsis
+        })
+            // .then(res => this.loadBooks())
             .catch(err => console.log(err));
     };
 
     onComponentMounted() {
-        const bookInfo = {id: this.props.id, title: this.props.title, authors: this.props.authors, thumbnail: this.props.thumbnail, synopsis: this.props.synopsis};
-        this.setState({book: bookInfo});
+        const bookInfo = { id: this.props.id, title: this.props.title, authors: this.props.authors, thumbnail: this.props.thumbnail, synopsis: this.props.synopsis };
+        this.setState({ book: bookInfo });
     }
-    
+
     render() {
-      return  (
-        <div className="book mx-auto">
-            <h2>{this.props.title} by {this.props.authors}</h2>
-            <img className="img-fluid" src={this.props.thumbnail} alt="Book Jacket" />
-            <br/>
-            <button type="button" className="btn btn-success mb-2 mt-2" onClick={this.details}>Details</button>
-            <button type="button" className="btn btn-success mb-2 mt-2 ml-2" onClick={this.handleBookSave}>Save</button>
-            <p>{this.props.synopsis}</p>
-        </div>
-    )
-        }
+        return (
+            <div className="book mx-auto">
+                <h2>{this.props.title} by {this.props.authors}</h2>
+                <img className="img-fluid" src={this.props.thumbnail} alt="Book Jacket" />
+                <br />
+                <button type="button" className="btn btn-success mb-2 mt-2" onClick={this.bookDetails}>Details</button>
+                <button type="button" className="btn btn-success mb-2 mt-2 ml-2" onClick={this.handleBookSave}>Save</button>
+                <p>{this.props.synopsis}</p>
+            </div>
+        )
+    }
 
 };
 export default BookSearch;
